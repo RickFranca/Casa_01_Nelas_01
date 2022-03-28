38cf18c58faf36ec94ca99fb3fa89baae17bdfc2
Ricardo Santos <rickfranca_santos@yahoo.com.br>
2022-03-28 18:47:06 +0100

    Casa_01_Nelas_01

---
+++ data.js
@@ -0,0 +1,495 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-street_01",
+      "name": "Street_01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.986950979104492,
+        "pitch": -0.10312193169573902,
+        "fov": 1.2933209261323464
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.101126199537994,
+          "pitch": 0.11095324415390273,
+          "rotation": 0,
+          "target": "1-entrance"
+        },
+        {
+          "yaw": 2.50973422689324,
+          "pitch": 0.06772914012050357,
+          "rotation": 0,
+          "target": "2-street_02"
+        },
+        {
+          "yaw": -0.643924848066117,
+          "pitch": 0.1174445872587917,
+          "rotation": 0,
+          "target": "10-church-square"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-entrance",
+      "name": "Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.0750806007250624,
+        "pitch": -0.08056400913729611,
+        "fov": 1.2933209261323464
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.34401355591806926,
+          "pitch": 0.24418157273379215,
+          "rotation": 0,
+          "target": "3-kitchen"
+        },
+        {
+          "yaw": 2.418636148084701,
+          "pitch": 0.1593821811755891,
+          "rotation": 0,
+          "target": "0-street_01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-street_02",
+      "name": "Street_02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.554860711885361,
+        "pitch": -0.012890241461967378,
+        "fov": 1.2933209261323464
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8583500808329454,
+          "pitch": 0.16775886629894998,
+          "rotation": 0,
+          "target": "1-entrance"
+        },
+        {
+          "yaw": -1.3516167004658115,
+          "pitch": 0.1811679150243961,
+          "rotation": 0,
+          "target": "0-street_01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.5704313836481276,
+        "pitch": -0.048166205051892064,
+        "fov": 1.2933209261323464
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.076079485206595,
+          "pitch": 0.10202513793017509,
+          "rotation": 0,
+          "target": "5-loft_01"
+        },
+        {
+          "yaw": -1.455207251417285,
+          "pitch": 0.13254148484951145,
+          "rotation": 0,
+          "target": "4-toilet"
+        },
+        {
+          "yaw": -2.7538449930523594,
+          "pitch": 0.12049321063756224,
+          "rotation": 0,
+          "target": "1-entrance"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-toilet",
+      "name": "Toilet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.0533470914804433,
+        "pitch": 0.38348468349353126,
+        "fov": 1.2933209261323464
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0618184383439946,
+          "pitch": 0.366509145095538,
+          "rotation": 0,
+          "target": "3-kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-loft_01",
+      "name": "Loft_01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.397011791266495,
+        "pitch": 0.006445120730983689,
+        "fov": 1.2933209261323464
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08257170920362711,
+          "pitch": 0.5737619471021844,
+          "rotation": 0,
+          "target": "3-kitchen"
+        },
+        {
+          "yaw": -0.8211964173230193,
+          "pitch": 0.09196572379380363,
+          "rotation": 0,
+          "target": "6-loft_02"
+        },
+        {
+          "yaw": -2.068972799786735,
+          "pitch": 0.12657968475548032,
+          "rotation": 0,
+          "target": "8-bathroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-loft_02",
+      "name": "Loft_02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.548640945737624,
+        "pitch": 0.0290030432894266,
+        "fov": 1.2933209261323464
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8283074566763773,
+          "pitch": 0.1324778782841225,
+          "rotation": 0,
+          "target": "5-loft_01"
+        },
+        {
+          "yaw": -0.15250952554602293,
+          "pitch": 0.14342163068442204,
+          "rotation": 0,
+          "target": "8-bathroom"
+        },
+        {
+          "yaw": 1.1940633098612174,
+          "pitch": 0.15816960993140938,
+          "rotation": 0,
+          "target": "7-bedroom"
+        },
+        {
+          "yaw": 1.5480016707053315,
+          "pitch": 0.18641618718654485,
+          "rotation": 0,
+          "target": "9-balcony"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-bedroom",
+      "name": "Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.6400317497791033,
+        "pitch": -0.014267244401029444,
+        "fov": 1.2933209261323464
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.223412595015306,
+          "pitch": 0.24596138646422716,
+          "rotation": 0,
+          "target": "6-loft_02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-bathroom",
+      "name": "Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.9515895878047544,
+        "pitch": 0.08699924355707367,
+        "fov": 1.2933209261323464
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9345884306709102,
+          "pitch": 0.16257825750614074,
+          "rotation": 0,
+          "target": "5-loft_01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-balcony",
+      "name": "Balcony",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.7016630526227576,
+        "pitch": -0.07717754592303905,
+        "fov": 1.2933209261323464
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.156844977674087,
+          "pitch": 0.20176781974780056,
+          "rotation": 0,
+          "target": "6-loft_02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-church-square",
+      "name": "Church Square",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.9044520687321373,
+        "pitch": 0.23192163018847545,
+        "fov": 1.2933209261323464
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8808036831914565,
+          "pitch": 0.024239235530515657,
+          "rotation": 0,
+          "target": "0-street_01"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
